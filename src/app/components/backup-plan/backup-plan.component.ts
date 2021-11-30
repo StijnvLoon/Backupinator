a124856bf8cdc1d6dbb6633a02d429d0341376fc
StijnvLoon <stijnvanloon@home.nl>
2021-11-30 23:22:29 +0100

    make backup button

--- src/app/components/backup-plan/backup-plan.component.ts
+++ src/app/components/backup-plan/backup-plan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BackupPlan } from 'src/app/models/BackupPlan';
 import { BackupPlanService } from 'src/app/services/backup-plan.service';
 
@@ -9,13 +9,54 @@ import { BackupPlanService } from 'src/app/services/backup-plan.service';
 })
 export class BackupPlanComponent {
 
+  private progress: Progress
+
   constructor(private backupPlanService: BackupPlanService) { }
 
   getSelectedPlan(): BackupPlan {
     return this.backupPlanService.selectedBackupPlan
   }
 
+  startBackup() {
+    this.progress = new Progress()
+    const interval = setInterval(() => {
+      if(this.progress.index == this.progress.total) {
+        this.progress = undefined
+        clearInterval(interval)
+      } else {
+        this.progress.index += 1
+      }
+    }, 2000)
+  }
+
   updateSelectedPlan() {
     console.log('updated!')
   }
+
+  getLoaderText(): string {
+    if(this.progress) {
+      const percentage = Math.round((this.progress.index / this.progress.total * 100) * 1) / 1
+      return `File ${this.progress.index}/${this.progress.total} (${percentage}%)`
+    } else {
+      return 'Make Backup'
+    }
+  }
+
+  getLoaderStyle(): object {
+    if(this.progress) {
+      const width: string = (this.progress.index / this.progress.total * 100) + '%'
+
+      return {
+        'width': width
+      }
+    } else {
+      return {'width': '0%'}
+    }
+  }
+}
+
+class Progress {
+  public fileName: string = ''
+  public index: number = 0
+  public total: number = 3
 }
